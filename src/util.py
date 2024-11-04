66e877562d9dbebcbd1fb56e92da4fb3d694aa08
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-11-04 18:56:49 +0000

    Modify util
    * Add `Convertor`
    * Rename arg of `new_name`

--- src/util.py
+++ src/util.py
@@ -1,3 +1,4 @@
+import abc
 import itertools
 from typing import Collection
 
@@ -7,14 +8,22 @@ def flatten(ll: Collection) -> list:
     return list(itertools.chain(*ll))
 
 
-def new_name(seed: str, names: Collection[str]):
-    """Generate a new name that is not in `names` starting with `seed`.
+def new_name(prefix: str, names: Collection[str]):
+    """Generate a new name that is not in `names` starting with `prefix`.
     """
     i = 1
     while True:
-        name = f"{seed}{i}"
+        name = f"{prefix}{i}"
         if name not in names:
             return name
         i += 1
 
-
+class Convertor(metaclass = abc.ABCMeta):
+    @classmethod
+    @abc.abstractmethod
+    def convert(self, from_: object) -> object:
+        """Abstract method to convert an object
+        from `from_` to the return object
+        """
+        return NotImplemented
+    
