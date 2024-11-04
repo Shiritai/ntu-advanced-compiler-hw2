96d23eaca1f356ee87b82813cf6cacc2ecd15408
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-11-04 18:58:55 +0000

    Implement dominance

--- src/dominance.py
+++ src/dominance.py
@@ -1,32 +1,82 @@
-from typing import Dict, Set
+from collections import deque
+from functools import reduce
+from typing import Optional
 from cfg import CFG, BasicBlock
+from logger.logger import logger
+from util import Convertor
 
-class DominatorTree:
-    def __init__(self, cfg: CFG):
-        self.cfg = cfg
-        self.dom: Dict[BasicBlock, Set[BasicBlock]] = {}
-        self.idom: Dict[BasicBlock, BasicBlock] = {}
-        self.dom_frontiers: Dict[BasicBlock, Set[BasicBlock]] = {}
-        self.compute_dominators()
-        self.compute_dominance_frontiers()
+class Cfg2Dom(Convertor):
+    @classmethod
+    def convert(cls, cfg: CFG) -> dict[BasicBlock, set[BasicBlock]]:
+        """Convert cfg to Dominance: dict in format `BasicBlock : set of BasicBlock` pair
 
-    def compute_dominators(self):
-        """
         Computes the dominators for each basic block.
         """
         # TODO: Implement the iterative algorithm to compute dominators.
-        pass
+        full_set = lambda: set(cfg.blocks.values())
+        dom = { bb: full_set() for bb in cfg.blocks.values() }
+        while True:
+            changed = False
+            for bb in cfg.blocks.values():
+                nxt_dom = { bb }
+                if len(bb.preds) > 0:
+                    nxt_dom.update(reduce(lambda s1, s2: s1.intersection(s2),
+                                          list(dom.get(pred) for pred in bb.preds)))
 
-    def compute_idom(self):
+                if dom[bb] != nxt_dom:
+                    dom[bb] = nxt_dom
+                    changed = True
+                
+            if not changed:
+                break
+        return dom
+
+class Dom2Idom(Convertor):
+    @classmethod
+    def convert(cls,
+                 dom: dict[BasicBlock, set[BasicBlock]]) -> dict[BasicBlock, Optional[BasicBlock]]:
         """
         Computes the immediate dominator for each basic block.
         """
         # TODO: Compute immediate dominators based on the dominator sets.
-        pass
+        idom: dict[BasicBlock, Optional[BasicBlock]] = {}
+        for (bb, bb_dom) in dom.items():
+            if len(bb.preds) == 0:  # first block
+                idom[bb] = None
+            else:
+                bfs = deque((bb,))
+                seen = set(bfs)
+                while len(bfs) != 0:
+                    pred = bfs.popleft()
+                    if pred != bb and pred in bb_dom:
+                        # found idom
+                        idom[bb] = pred
+                        break
+                    bfs.extend(p for p in pred.preds if p not in seen)
+                    seen.update(pred.preds)
+        return idom
 
-    def compute_dominance_frontiers(self):
+class Idom2Df(Convertor):
+    @classmethod
+    def convert(cls, idom: dict[BasicBlock, Optional[BasicBlock]]):
         """
         Computes the dominance frontiers for each basic block.
         """
         # TODO: Implement dominance frontier computation.
-        pass
+        df: dict[BasicBlock, set[BasicBlock]] = {
+            bb: set() for bb in idom.keys() }
+        for bb in idom.keys():
+            if len(bb.preds) > 1:
+                for pred in bb.preds:
+                    cur = pred
+                    while cur != idom[bb]:
+                        df[cur].add(bb)
+                        cur = idom[cur]
+        return df
+
+class DominatorTree:
+    def __init__(self, cfg: CFG):
+        self.cfg = cfg
+        self.dom = Cfg2Dom.convert(self.cfg)
+        self.idom = Dom2Idom.convert(self.dom)
+        self.dom_frontiers = Idom2Df.convert(self.idom)
